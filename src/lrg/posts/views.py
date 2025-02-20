ad6937ae023e859a4113706e298f81ae35f8f4c9
jayvivor <jayvivor@gmail.com>
2025-02-20 03:59:29 -0600

    optimize search

--- src/lrg/posts/views.py
+++ src/lrg/posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery
 from django.http import JsonResponse
 
 from community.const import GAME_FORMATS
@@ -35,52 +35,45 @@ def info(request, post_id):
     return render(request, "posts/info.html", context={"post":post})
 
 def search(request):
+    context = {
+        "countries": COUNTRIES.items(),
+        "formats": GAME_FORMATS,
+        "binary_filters": [("Filmed only", "filmed")],
+    }
+
     if request.method == "POST":
         country_short = request.POST.get("countrySelected", None)
         city = request.POST.get("citySelected", None)
+        game_format = request.POST.get("gameFormat", None)
+        query = request.POST.get("query", None)
 
+        # Optimize location filtering
         locations = Location.objects.all()
-
         if country_short:
-            locations = Location.objects.filter(country_short=country_short)
+            locations = locations.filter(country_short=country_short)
         if city:
             city = city.split(",")[0]
             locations = locations.filter(city=city)
 
-        game_format = request.POST.get("gameFormat", None)
-
-        query = request.POST.get("query", None)
-        posts = Post.objects.all()
+        # Optimize post filtering
+        posts = Post.objects.all().select_related("season").prefetch_related("season__location")
 
         if game_format:
             posts = posts.filter(season__format=game_format)
 
         if query:
             posts = posts.annotate(
-                search=SearchVector("description","season__name","season__format"),
-            ).filter(search=query)
+                search=SearchVector("description", "season__name", "season__format")
+            ).filter(search=SearchQuery(query))
 
         if country_short or city:
-            posts = posts.filter(season__location__in=locations)
+            locations_list = locations.values_list("id", flat=True)
+            posts = posts.filter(season__location_id__in=locations_list)
 
+        context.update({"posts": posts})
 
-        return render(request, 'posts/search.html', context={
-            "countries": COUNTRIES.items(),
-            "formats": GAME_FORMATS,
-            "binary_filters": [
-                ("Filmed only", "filmed"),
-            ],
-            "posts":posts,
-            "info":[country_short, city, str(locations), query],
-        })
+    return render(request, "posts/search.html", context)
 
-    return render(request, 'posts/search.html', context={
-        "countries": COUNTRIES.items(),
-        "formats": GAME_FORMATS,
-        "binary_filters": [
-            ("Filmed only", "filmed"),
-        ]
-    })
 
 # AJAX
 
