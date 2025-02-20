2f469f872985f4f91ea1f675630d8ce3940a7942
Jayvivor <jayvivor@gmail.com>
2025-02-20 13:06:53 -0600

    fixed reset script

--- src/lrg/scripts/full_reset.sh
+++ src/lrg/scripts/full_reset.sh
@@ -2,17 +2,17 @@ rm -rf ./*/migrations/0*
 set -e
 
 export OWNER=lrg_demo_owners
-export PGPASSWORD=$RDS_PASSWORD
-export PGHOST=$RDS_HOSTNAME
-export PGUSER=$RDS_USERNAME
-export PGPORT=$RDS_PORT
+export PGPASSWORD=$DB_PASSWORD
+export PGHOST=$DB_HOSTNAME
+export PGUSER=$DB_USERNAME
+export PGPORT=$DB_PORT
 
 # Terminate active connections
-psql -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname = '${RDS_DB_NAME}' AND pid <> pg_backend_pid();"
+psql -d postgres -c "SELECT pg_terminate_backend(pg_stat_activity.pid) FROM pg_stat_activity WHERE datname = '${DB_NAME}' AND pid <> pg_backend_pid();"
 
 # Drop and recreate the database
-psql -d postgres -c "DROP DATABASE IF EXISTS ${RDS_DB_NAME};"
-psql -d postgres -c "CREATE DATABASE ${RDS_DB_NAME} OWNER ${PGUSER};"
+psql -d postgres -c "DROP DATABASE IF EXISTS ${DB_NAME};"
+psql -d postgres -c "CREATE DATABASE ${DB_NAME} OWNER ${PGUSER};"
 
 python manage.py flush --no-input
 python manage.py makemigrations && python manage.py migrate
